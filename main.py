765e89b277bd928a93d676484835db56bdc3a9c3
Aparna <shrivastava.aparna01@gmail.com>
2023-11-06 16:04:48 +0530

    variable added

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     name= 'try2catch'
+    address = 'india'
     print_hi('hello Aparna:{name}')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
